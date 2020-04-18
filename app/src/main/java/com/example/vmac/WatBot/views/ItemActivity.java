77ba280defee906542405c74c27fc89a4cc228f3
danialaa <d.a.mahdaly@outlook.com>
2020-04-19 01:23:15 +0200

    Added Item Page

---
+++ app/src/main/java/com/example/vmac/WatBot/views/ItemActivity.java
@@ -0,0 +1,40 @@
+package com.example.vmac.WatBot.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.vmac.WatBot.R;
+
+public class ItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+    }
+
+    public void goToWishList(View view) {
+    }
+
+    public void goToProfile(View view) {
+    }
+
+    public void backToInitial(View view) {
+    }
+
+    public void addToWishlist(View view) {
+    }
+
+    public void goToMap(View view) {
+    }
+
+    public void proceedItem(View view) {
+    }
+
+    public void viewSecondItem(View view) {
+    }
+
+    public void viewThirdItem(View view) {
+    }
+}
